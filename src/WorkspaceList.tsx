d5b484231ad5e80d348420d3a925783bb03c8c0e
noahlibeskind <noahlibeskind@gmail.com>
2023-02-20 19:33:17 -0800

    displaying messages with members names

--- src/WorkspaceList.tsx
+++ src/WorkspaceList.tsx
@@ -1,134 +1,218 @@
 import React, { useEffect, useState } from "react";
 
 interface WSListProps {
-    // setSelectedWorkSpace: React.Dispatch<React.SetStateAction<string>>;
-    loggedInUser: any;
-    setLoggedInUser: React.Dispatch<React.SetStateAction<any>>;
+  // setSelectedWorkSpace: React.Dispatch<React.SetStateAction<string>>;
+  loggedInUser: any;
+  setLoggedInUser: React.Dispatch<React.SetStateAction<any>>;
+}
+
+interface Member {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  accessToken: string
 }
 
 interface Workspace {
-    id: string;
-    name: string;
-    channels: number;
-    owner: string;
+  id: string;
+  name: string;
+  channels: number;
+  owner: string;
 }
 
 interface Channel {
-    id: string;
-    name: string;
-    messages: number;
+  id: string;
+  name: string;
+  messages: number;
+}
+
+interface Message {
+  id: string;
+  member: string;
+  posted: Date;
+  content: string;
 }
 
 const WorkspaceList: React.FC<WSListProps> = ({ loggedInUser, setLoggedInUser }) => {
-    const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
-    const [channels, setChannels] = useState<Channel[]>([]);
+  const [members, setMembers] = useState<Member[]>([])
+  const [memberMap, setMemberMap] = useState<{[key: string]: string}>({})
+  const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const [channels, setChannels] = useState<Channel[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
 
-    const [currWorkspace, setCurrWorkspace] = useState<Workspace>();
-    const [currChannel, setCurrChannel] = useState<Channel>();
 
-    useEffect(() => {
-        fetch("http://10.0.0.57:8080/workspace", {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + loggedInUser.accessToken,
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Failed to fetch workspaces");
-            }
-        })
-        .then(data => setWorkspaces(data))
-        .catch(error => console.error(error));
-    }, [loggedInUser]);
+  const [currWorkspace, setCurrWorkspace] = useState<Workspace>();
+  const [currChannel, setCurrChannel] = useState<Channel>();
 
-    useEffect(() => {
-        fetch("http://10.0.0.57:8080/workspace/channel/" + currWorkspace?.id, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + loggedInUser.accessToken,
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Failed to fetch channels");
-            }
-        })
-        .then(data => setChannels(data))
-        .catch(error => console.error(error));
-    }, [currWorkspace, channels])
+  useEffect(() => {
+      fetch("http://10.0.0.57:8080/workspace", {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + loggedInUser?.accessToken,
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch workspaces");
+        }
+      })
+      .then(data => setWorkspaces(data))
+      .catch(error => console.error(error));
 
-    return (
-        <div className="tile is-ancestor">
-            {/* Workspaces */}
-            <div className="tile is-parent">
-                {/* <div className="tile"> */}
-                    <div className="tile is-child">
-                        <p className="subtitle">Workspaces</p>
-                    </div>
-                {/* </div> */}
-                <ul>
-                    {workspaces.map(workspace => (
-                        <li key={workspace.id}>
-                            <div className="tile is-parent">
-                            {workspace.id === currWorkspace?.id ?
-                            <div className="tile is-child notification is-link">
-                                <p className="subtitle">
-                                    {workspace.name}
-                                </p>
-                            </div>
-                            :
-                            <div className="tile is-child box" onClick={() => setCurrWorkspace(workspace)}>
-                                <p className="subtitle">
-                                    {workspace.name}
-                                </p>
-                            </div>
-                            }
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            {/* Channels */}
-            <div className="tile is-parent">
-                <div className="tile">
-                    <div className="tile is-child">
-                        <p className="subtitle">Channels</p>
-                    </div>
+      fetch("http://10.0.0.57:8080/member", {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + loggedInUser?.accessToken,
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch members");
+        }
+      })
+      .then(data => {
+        setMembers(data);
+        for (const member of data) {
+          console.log(member)
+          setMemberMap((prevMemberMap) => ({...prevMemberMap, [member.id]: member.name}))
+        }
+      })
+      .catch(error => console.error(error));
+
+      
+  }, [loggedInUser]);
+
+  useEffect(() => {
+    fetch("http://10.0.0.57:8080/workspace/channel/" + currWorkspace?.id, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + loggedInUser?.accessToken,
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch channels");
+      }
+    })
+    .then(data => setChannels(data))
+    .catch(error => console.error(error));
+  }, [loggedInUser, currWorkspace, channels])
+
+  useEffect(() => {
+    fetch("http://10.0.0.57:8080/channel/message/" + currChannel?.id, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + loggedInUser?.accessToken,
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch messages");
+      }
+    })
+    .then(data => setMessages(data))
+    .catch(error => console.error(error));
+  }, [loggedInUser, currChannel, messages])
+
+  return (
+      <div className="tile is-ancestor">
+        {/* Workspaces */}
+        <div className="tile is-parent is-vertical is-2">
+          <div className="tile is-child">
+            <p className="subtitle">Workspaces</p>
+          </div>
+          <ul>
+            {workspaces.map(workspace => (
+              <li key={workspace.id}>
+                <div className="tile is-parent">
+                {workspace.id === currWorkspace?.id ?
+                <div className="tile is-child notification is-link">
+                  <p className="is-size-6">
+                      {workspace.name}
+                  </p>
                 </div>
-                <ul>
-                    {channels.map(channel => (
-                        <li key={channel.id}>
-                            <div className="tile is-parent">
-                            {channel.id === currChannel?.id ?
-                            <div className="tile is-child notification is-link">
-                                <p className="subtitle">
-                                    {channel.name}
-                                </p>
-                            </div>
-                            :
-                            <div className="tile is-child box" onClick={() => setCurrChannel(channel)}>
-                                <p className="subtitle">
-                                    {channel.name}
-                                </p>
-                            </div>
-                            }
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                :
+                <div className="tile is-child notification" onClick={() => setCurrWorkspace(workspace)}>
+                  <p className="is-size-6">
+                      {workspace.name}
+                  </p>
+                </div>
+                }
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-            
-    );
+        {/* Channels */}
+        <div className="tile is-parent is-vertical is-2">
+          <div className="tile is-child">
+              <p className="subtitle">Channels</p>
+          </div>
+        
+          <ul>
+            {channels.map(channel => (
+              <li key={channel.id}>
+                <div className="tile is-parent">
+                {channel.id === currChannel?.id ?
+                  <div className="tile is-child notification is-link">
+                    <p className="is-size-6">
+                        {channel.name}
+                    </p>
+                  </div>
+                  :
+                  <div className="tile is-child notification" onClick={() => setCurrChannel(channel)}>
+                    <p className="is-size-6">
+                        {channel.name}
+                    </p>
+                  </div>
+                }
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Messages */}
+        <div className="tile is-parent is-vertical is-8">
+          <div className="tile is-child">
+            <p className="subtitle">Messages</p>
+          </div>
+          <ul>
+            {messages.map(message => (
+              <li key={message.id}>
+                <div className="tile is-parent">
+                  <div className="tile is-child notification">
+                    <p className="is-size-6">
+                      {message.content}
+                    </p>
+                    <p className="is-size-7">
+                      {memberMap[message.member]}
+                    </p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+          
+  );
 };
 
 export default WorkspaceList;
